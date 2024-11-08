ec1f0267593efcb8f5ae233acd55cd5d6b909e4e
ruth-ioffe <ruth.ioffe.study@gmail.com>
2024-11-08 14:35:19 +0200

    did the isEmpty func and the isFull func. started doing the enqueue func

--- hw1/Queue.cpp
+++ hw1/Queue.cpp
@@ -20,10 +20,45 @@ void cleanQueue(Queue* q)
 
 void enqueue(Queue* q, unsigned int newValue)
 {
-
+	if (!isFull(q))
+	{
+		q->elements.push(newValue);
+	}
 }
 
 int dequeue(Queue* q); // return element in top of queue, or -1 if empty
 
-bool isEmpty(Queue* s);
-bool isFull(Queue* s);
+
+bool isEmpty(Queue* s)
+/*
+this function checks if the queue is empty
+input: Queue* s
+output: true or false (if the queue is empty or not)
+*/
+{
+	if (s->count == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isFull(Queue* s)
+/*
+this function checks if the queue is full
+input: Queue* s
+output: true or false (if the queue is full or not)
+*/
+{
+	if (s->count == s->maxSize)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
