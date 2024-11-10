d767a021f9bdac574bbcc5da82aebcfbd14e9806
ruth-ioffe <ruth.ioffe.study@gmail.com>
2024-11-10 19:16:55 +0200

    finished the ququ main1 checker. now need to see of the queue is ok

--- hw1/main1.cpp
+++ hw1/main1.cpp
@@ -5,10 +5,37 @@ void printQueue(Queue* q);
 
 int main()
 {
+	Queue* q = new Queue;
+	int number = 0;
 
+	initQueue(q, 5);
+	enqueue(q, 1);
+	enqueue(q, 2);
+	enqueue(q, 3);
+	enqueue(q, 4);
+	enqueue(q, 5);
+	printQueue(q);
+	number = dequeue(q);
+	if (number != -1)
+	{
+		std::cout << "Number removed: " << number << std::endl;
+	}
+	else
+	{
+		std::cout << "Queue Empty!" << std::endl;
+	}
+	std::cout << "" << std::endl;
+	printQueue(q);
 }
 
 void printQueue(Queue* q)
 {
+	int i = 0;
+	std::cout << "the current queue: ";
 
+	for (i = 0; i < q->count; i++)
+	{
+		std::cout << q->elements[i] << " ";
+	}
+	std::cout << "" << std::endl;
 }
